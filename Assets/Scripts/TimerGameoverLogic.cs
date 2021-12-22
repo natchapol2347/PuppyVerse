cf35db90a73c12365f3c06b783536d54b3b3c458
Wloha <72493130+Wloha@users.noreply.github.com>
2021-12-22 22:16:53 +0700

    feat: Timer

---
+++ Assets/Scripts/TimerGameoverLogic.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerGameoverLogic : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    int countDownStartValue = 100;
+    public TMP_Text timerUI;
+    void Start()
+    {
+        counDownTimer();
+    }
+
+    void counDownTimer()
+    {
+        if (countDownStartValue > 0)
+        {
+            TimeSpan spanTime = TimeSpan.FromSeconds(countDownStartValue);
+            if (spanTime.Seconds < 10)
+            {
+                timerUI.text = "Timer: " + spanTime.Minutes + ":0" + spanTime.Seconds;
+            }
+            else { 
+                timerUI.text = "Timer: " + spanTime.Minutes + ":" + spanTime.Seconds; 
+            }
+            countDownStartValue--;
+            Invoke("counDownTimer", 1.0f);
+        }
+
+        else
+        {
+            timerUI.text = "GameOver!";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
